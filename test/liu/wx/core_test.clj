936d5f0dd267d7b4d92879e96543610ff14d00d8
刘宇辉 <emliunix@clojurians.org>
2016-07-30 09:08:44 +0800

    完成 verifyurl-handler 的测试用例

--- test/liu/wx/core_test.clj
+++ test/liu/wx/core_test.clj
@@ -8,13 +8,14 @@
 (def token "emtoken")
 (def aes-key "DOZEPQXatVT1RbgvP4nFuACCoixAdPlAR1T51aU33c4")
 (def query "signature=2f3200ec27097e9d7880e14db97cff16e4a6b86d&echostr=8197925266815712683&timestamp=1469801643&nonce=1972100835")
+(def echostr-in-query "8197925266815712683")
 
 (def mock-req
   {:request-method :get
    :query-string query})
 
-(deftest wx-verifyurl-handler
-  (is (= {:body "test"}
+(deftest wx-verifyurl-handler-test
+  (is (= {:body echostr-in-query}
          ((-> t/wx-verifyurl-handler
               (wrap-wx-params
                :token token
