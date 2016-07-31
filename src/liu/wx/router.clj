30377e40e5e54785f224fb8c20b29dfeea3d3202
刘宇辉 <emliunix@clojurians.org>
2016-08-01 00:46:34 +0800

    添加 router骨架

--- src/liu/wx/router.clj
+++ src/liu/wx/router.clj
@@ -1,2 +1,30 @@
 (ns liu.wx.router)
 
+;; route format
+;; event handler
+
+(defn- get-msg-type [wx-data]
+  (->
+   (let [msg-type (:MsgType wx-data)]
+     (if (= "event" msg-type)
+       (let [event-type (:Event wx-data)]
+         (if (and
+              (= "subscribe" event-type)
+              (not (nil? (:EventKey wx-data))))
+           ;; 是一个subscribe-and-scan事件
+           :subscribe-and-scan
+           ;; 否则直接返回event-type
+           event-type))
+       msg-type))
+   ;; 检查结果是不是nil，如果是nil，转换为:invalid
+   ;; 否则传入keyword保证返回的是一个keyword
+   ((fn [msg-type]
+      (if (nil? msg-type)
+        :invalid
+        (keyword msg-type))))))
+
+(defn router [handlers]
+  (fn [req]
+    (let [wx-data (:wx-data req)
+          event-type (get-msg-type wx-data)]
+      )))
